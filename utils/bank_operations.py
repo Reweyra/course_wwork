4c674cdae657182ccb43ae66aa95b3fb0aaaf279
Arkadiy <agongadze8@gmail.com>
2024-02-18 10:31:29 +0300

    all functionality implemented

--- utils/bank_operations.py
+++ utils/bank_operations.py
@@ -8,6 +8,9 @@ with open('../operations.json', 'r', encoding='utf-8') as file:
 
 transactions = []
 
+"""
+Распарсил данные JSON в список объектов Transaction
+"""
 for operation_data in data:
     transaction = Transaction(operation_data["id"],
                               operation_data["date"],
@@ -19,6 +22,10 @@ for operation_data in data:
                               operation_data.get("to")
                               )
     transactions.append(transaction)
+
+"""
+Отфильтровал только те операции которые прошли успешно
+"""
 executed_transactions = [transaction for transaction in transactions if transaction.state == "EXECUTED"]
 sorted_transactions = sorted(executed_transactions, key=lambda x: datetime.strptime(x.date, "%Y-%m-%dT%H:%M:%S.%f"),
                              reverse=True)
